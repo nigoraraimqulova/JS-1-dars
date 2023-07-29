b112e651f8322fa68773bf0b313e82ce8113c406
Sardorikromovv <139213926+nigoraraimqulova@users.noreply.github.com>
2023-07-29 14:17:53 +0500

    js 1-dars
    22-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*console.log("Men console oynasida ko'rinaman!");*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*document.write("Men body oynasida ko'riaman!");*/
+
+/*let x = 2;*/
+/*let y = 7;*/
+/*x = 10;*/
+
+/*let x = 2, y = 5, z = 8;*/
+/*document.write(x+y+z);*/
+
+/*let salom = "Assalomu alaykum!";*/
+/*let rang = " #f1f1f1";*/
+/*document.write(rang);*/
+
+/* Data types in JS */
+/*let x = 5;*/               /* numder */
+/*let matn = "JavaScript"*/    /* string */
+/*document.write( typeof matn);*/
+
+/* Matn in JS */
+/*let x = 5;*/
+/*let matn = "19"*/
+/*x+=1;*//* x=5+ */
+/*x-=1;*//* x=5-1 */
+/*x*=1;*//* x=5*1 */
+/*x/=1;*//* x=5/1 */
+/*let qoldiq = x%4;*/
+/*let daraja = x**2;*/ /* 5*5 */
+/*++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x+matn);*/
+
+/* if (agar) */
+/* else (u holda) */
+/* else if (yoki bo'lmasa) */
+
+/*if ("salom" == "salom"){
+    console.log("Ular teng!");
+}*/
+/*else {
+    console.log("Ular teng emas!")
+}*/
+
+if (45 == 44){
+    console.log("Birnchisidagi qiymatlar teng!");
+}
+else if (45 == 46){
+    console.log("Ikkinchisidagi qiymatlar teng!");
+}
+else {
+    console.log("Ikkalasidagi qimatlar teng emas!");
+}
+
+
+
+
+
+
+
---
+++ js/main1.js
@@ -0,0 +1,23 @@
+/*if ( 10.79 == 10.78){
+    console.log("Birnchisidagi qiymatlar teng!");
+}
+else if (10.79 == 10.80){
+    console.log("Ikkinchisidagi qiymatlar teng!");
+}
+else {
+    console.log("Ikkalasidagi qimatlar teng emas!");
+}*/
+/*if ( "jesuismort" == "jesuismort"){
+    console.log("Ular teng!");
+}
+else {
+    console.log("Ular teng emas!");
+}*/
+
+let n = 19;
+
+let matn = "7"
+
+document.write(n+matn)
+
+
